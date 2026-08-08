699a6f950cfa82ced3f8f750f4b5a9916a0de67a
007plato <130290301+007plato@users.noreply.github.com>
2026-08-08 22:43:13 +0800

    1.5
    修复一些已知问题...

--- src/main.jsx
+++ src/main.jsx
@@ -1209,6 +1209,7 @@ function Projects() {
                   loading="lazy"
                   fetchPriority="low"
                   decoding="async"
+                  onClick={togglePlay}
                 />
               )}
 
